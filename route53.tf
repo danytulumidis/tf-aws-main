89744daf67cd9be3e6aa5908b0239da8112bca7c
dtulumidis <dextavision@gmail.com>
2020-12-08 17:43:33 +0100

    new a and cname record to host my website via github pages

--- route53.tf
+++ route53.tf
@@ -1,3 +1,19 @@
 resource "aws_route53_zone" "danytulumidis" {
   name = "danytulumidis.com"
 }
+
+resource "aws_route53_record" "danytulumidis_cname" {
+  zone_id = aws_route53_zone.danytulumidis.id
+  name = "www.danytulumidis.com"
+  type = "CNAME"
+  ttl = "300"
+  records = ["dextavision.github.io"]
+}
+
+resource "aws_route53_record" "danytulumidis_alias" {
+  zone_id = aws_route53_zone.danytulumidis.id
+  name = "danytulumidis.com"
+  type = "A"
+  ttl = "60"
+  records = ["185.199.108.153","185.199.109.153","185.199.110.153","185.199.111.153"]
+}
